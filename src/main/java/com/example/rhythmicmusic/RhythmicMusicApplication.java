ba2bb27b3cb4e0b6dbbc2f0131490e1ead8a0a69
SGJ <2096177261@qq.com>
2023-05-21 19:43:27 +0800

    add

---
+++ src/main/java/com/example/rhythmicmusic/RhythmicMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.rhythmicmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhythmicMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhythmicMusicApplication.class, args);
+    }
+
+}
