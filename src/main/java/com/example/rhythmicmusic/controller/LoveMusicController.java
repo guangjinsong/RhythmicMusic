34c4ce0ec1a34bfa7f65d633e7796d4101304770
SGJ <2096177261@qq.com>
2023-06-03 20:11:38 +0800

    add

---
+++ src/main/java/com/example/rhythmicmusic/controller/LoveMusicController.java
@@ -0,0 +1,76 @@
+package com.example.rhythmicmusic.controller;
+
+import com.example.rhythmicmusic.mapper.LoveMusicMapper;
+import com.example.rhythmicmusic.model.Music;
+import com.example.rhythmicmusic.model.User;
+import com.example.rhythmicmusic.tools.Constant;
+import com.example.rhythmicmusic.tools.ResponseBodyMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ * @author sgj
+ * @create 2023-06-03 14:45
+ */
+@RestController
+@RequestMapping("/lovemusic")
+public class LoveMusicController {
+    @Autowired
+    private LoveMusicMapper loveMusicMapper;
+
+    @RequestMapping("/likemusic")
+    public ResponseBodyMessage<Boolean> likeMusic(@RequestParam String id, HttpServletRequest request) {
+        int musicId = Integer.parseInt(id);
+        // 1. 检查是否登录
+        HttpSession httpSession = request.getSession(false); // false的意思是,如果没有session就不创建
+        if (null == httpSession || null == httpSession.getAttribute(Constant.USERINFO_SESSION_KEY)) {
+            System.out.println("没有登录");
+            return new ResponseBodyMessage<>(-1, "请登录后上传", false);
+        }
+
+        User user = (User)httpSession.getAttribute(Constant.USERINFO_SESSION_KEY);
+        int userId = user.getId();
+        if (loveMusicMapper.findLoveMusicByUserIdAndMusicId(userId, musicId) != null) {
+            // 优化：取消收藏该音乐
+            return new ResponseBodyMessage<>(-1, "您之前已收藏过该音乐", false);
+        } else {
+            if (loveMusicMapper.insertLoveMusic(userId, musicId)) {
+                return new ResponseBodyMessage<>(1, "收藏成功", true);
+            } else {
+                return new ResponseBodyMessage<>(-1, "收藏失败", true);
+            }
+        }
+    }
+
+    @RequestMapping("/findlovemusic")
+    public ResponseBodyMessage<List<Music>> findLoveMusic(@RequestParam(required = false) String musicName, HttpServletRequest request) {
+        // 1. 检查是否登录
+        HttpSession httpSession = request.getSession(false); // false的意思是,如果没有session就不创建
+        if (null == httpSession || null == httpSession.getAttribute(Constant.USERINFO_SESSION_KEY)) {
+            System.out.println("没有登录");
+            return new ResponseBodyMessage<>(-1, "请登录后上传", null);
+        }
+
+        User user = (User)httpSession.getAttribute(Constant.USERINFO_SESSION_KEY);
+        int userId = user.getId();
+
+        List<Music> ans = null;
+        if (musicName == null) {
+            System.out.println("1");
+            ans  = loveMusicMapper.findLoveMusicByUserId(userId); // 查询该用户所有的音乐
+        } else {
+            System.out.println("2");
+            ans = loveMusicMapper.findLoveMusicByKeyAndUID(musicName, userId);
+            System.out.println(ans);
+        }
+
+        return new ResponseBodyMessage<>(1, "查询成功", ans);
+    }
+
+}
--- src/main/java/com/example/rhythmicmusic/mapper/LoveMusicMapper.java
+++ src/main/java/com/example/rhythmicmusic/mapper/LoveMusicMapper.java
@@ -3,6 +3,8 @@ package com.example.rhythmicmusic.mapper;
 import com.example.rhythmicmusic.model.Music;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * @author sgj
  * @create 2023-06-02 19:22
@@ -10,8 +12,19 @@ import org.apache.ibatis.annotations.Mapper;
 // 收藏音乐
 @Mapper
 public interface LoveMusicMapper {
+    // 查询某个用户收藏的某首音乐
+    Music findLoveMusicByUserIdAndMusicId(int userId, int musicId);
+
+
     // 收藏音乐
-    Music findLoveMusic(int userId, int musicId);
-    // 查询收藏音乐
     boolean insertLoveMusic(int userId, int musicId);
+
+    // 查询某个用户收藏的所有音乐
+    List<Music> findLoveMusicByUserId(int userId);
+
+    // 查询当前用户，指定为musicName的音乐相关信息，支持模糊查询
+    List<Music> findLoveMusicByKeyAndUID(String musicName, int userId);
+
+    // 取消收藏
+    int deleteLoveMusic(int userId, int musicId);
 }
